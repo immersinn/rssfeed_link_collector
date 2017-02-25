7db7e6f5c3b122e96e413d33176f229792a62fef
ims <immersinn@gmail.com>
2017-02-25 11:02:14 -0700

    + moved "get_XXXfeedlist" files to new folder + "formalizeD" the "local_collection" code (changed name from "build_local_grphy.py", hence the file delete); works for now... + updated "myqsl_utils.py" code with new queries used for the local collection builder code...

---
+++ src/local_collection.py
@@ -0,0 +1,134 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 23 22:06:12 2017
+
+@author: immersinn
+"""
+
+import mysql_utils
+import doc_proc
+import init_tdm_tables
+
+
+def ids2ints(id_series):
+    return([int(i) for i in list(id_series)])
+
+
+def get_localWordsAndDocs(doc,
+                          l01_ndoc_cutoff=100,
+                          l02_restrict={'n_docs_bg': {
+                                                      'max' : 50
+                                                      },
+                                        'n_docs_qw' : {
+                                                       'min' : 3
+                                                       },
+                                        'ratio' : 2
+                                        },
+                          verbose=False):
+    
+    if verbose:
+        print('Init-ing tools...')
+    
+    # Connections
+    cnx = mysql_utils.getCnx()
+    cur = mysql_utils.getCur(cnx)
+    
+    # Doc Proc Stuffs
+    dd = lambda doc: doc_proc.build_text_feature(doc, 
+                                                 components = ['title', 'summary'],
+                                                 lower=False, 
+                                                 remove_stops=False,
+                                                 html_text=True,
+                                                 )
+    dp = init_tdm_tables.DocProcessor()
+    stops = doc_proc.nltk_stops
+    stops_lookup = mysql_utils.query_wordIDLookup(cur, stops)
+    n_docs_tot = mysql_utils.query_totalEntries(cur, 'rssfeed_links')
+    
+    
+    # Level 0
+    if verbose:
+        print('Starting Level 0...')
+    bow = dp.doc2BOW(dd(doc))
+    orig_query_words = [w for w in \
+                       (mysql_utils.query_wordIDLookup(cur,
+                                                       [w for w in bow if \
+                                                        w not in doc_proc.nltk_stops])).keys()
+                       ]
+    
+    
+    # Level 1
+    if verbose:
+        print('Starting Level 1...')
+    ##ndoc_cutoff = 100
+    word_count_info = mysql_utils.query_wordSummaryInfo(cur, orig_query_words, wtype='id')
+    qw_l01 = ids2ints(word_count_info[word_count_info.n_docs < l01_ndoc_cutoff].word_id)
+    docs_l01, wcs_l01 = mysql_utils.query_docsOnWords(cur, qw_l01, word_type='id')
+
+    
+    # Level 2
+    if verbose:
+        print('Starting Level 2...')
+    query_words = mysql_utils.query_AllDocWords(cur, docs_l01)
+    query_words = [w for w in query_words if w not in stops_lookup.keys()]
+    
+    bgwi = mysql_utils.query_backgroundWordInfo(cur, query_words)
+    bgwi['frac'] = bgwi.n_docs / n_docs_tot
+    qwwi = mysql_utils.query_wordSummaryInfo(cur, query_words,
+                                             wtype='id', include_docs=docs_l01)
+    qwwi['frac'] = qwwi.n_docs / len(docs_l01)
+    wi = bgwi.join(qwwi, on=['word_id'], lsuffix='_bg', rsuffix='_qw')
+    wi['ratio'] = wi.frac_qw / wi.frac_bg
+    
+    # Restrict query words based on "words_info" table in mysql
+    # Say, more than 5 docs, but less than 100 (50) (20) docs?
+    word_filter_l02 = lambda x: (x['n_docs_qw'] >= 3 and x['n_docs_bg'] < 50 and x['ratio'] > 2)
+    qw_l02 = ids2ints(wi.word_id_bg[wi.apply(lambda entry: word_filter_l02(entry), axis=1)])
+    docs_l02, wcs_l02 = mysql_utils.query_docsOnWords(cur, qw_l02,
+                                                      word_type="id", 
+                                                      exclude_docs=docs_l01)
+
+    
+    # Collect info and return data
+    if verbose:
+        print('Collecting data, getting BOWs and finishing up...')
+    words = {'orig' : set(orig_query_words),
+             'l01' : set(qw_l01),
+             'l02' : set(qw_l02)
+             }
+    docs = {'orig' : doc,
+            'l01' : docs_l01,
+            'l02' : docs_l02}
+    
+    bows = {'orig' : bow}
+    for d in docs_l01:
+        bows[d] = mysql_utils.query_docBOW(cur, d, word_list=words)
+    for d in docs_l02:
+        bows[d] = mysql_utils.query_docBOW(cur, d, word_list=words)
+        
+    # Close conenction
+    cnx.close()
+        
+    return(docs, words, bows)
+    
+
+
+if __name__=="__main__":
+    
+    """
+    doc_url = "http://www.cnn.com/2017/02/23/politics/fbi-refused-white-house-request-to-knock-down-recent-trump-russia-stories/index.html"
+    """
+    
+    print("Starting process...")
+    
+    doc = {"title" : "FBI refused White House request to knock down recent Trump-Russia stories",
+           "summary" : "Washington (CNN) The FBI rejected a recent White House request to publicly knock down media reports about communications between Donald Trump's associates and Russians known to US intelligence during the 2016 presidential campaign, multiple US officials briefed on the matter tell CNN."}
+    print('Doc title:')
+    print('"""' + doc['title'] + '"""')
+    
+    docs, words, bows = get_localWordsAndDocs(doc, verbose=True)
+    print('Total documents found: {}'.format(len(bows)))
+    print('Total words to be used: {}'.format(sum([len(words[w]) for w in words])))
+    
+    print('fine')
--- src/mysql_utils.py
+++ src/mysql_utils.py
@@ -22,6 +22,11 @@ TABLES = ['rssfeed_links', 'words', 'doc_bows']
 TABLE = TABLES[0]
 
 
+########################################
+## General Helper functions
+########################################
+
+
 def getCnx():
     u,p = utils.get_creds('MySQL')
     cnx = mysql.connector.connect(user=u, password=p, database=DB)
@@ -99,7 +104,12 @@ def exists_base(cursor, entry):
         return(False)
     
     
-def query_docs_by_datetime(cursor, 
+########################################
+## Pre-hashed query functions
+########################################
+    
+    
+def query_docsByDatetime(cursor, 
                            start_dt, end_dt='Now',
                            fields = ['link', 'title', 'rss_link', 'summary', 'published']):
     
@@ -114,7 +124,7 @@ def query_docs_by_datetime(cursor,
     return(dfDocsFromCursor(cursor))
 
 
-def query_docs_details(cursor, doc_ids, 
+def query_docsDetails(cursor, doc_ids, 
                        fields=['link', 'title', 'summary', 'published']):
     
     format_strings = ','.join(['%s'] * len(doc_ids))
@@ -124,6 +134,144 @@ def query_docs_details(cursor, doc_ids,
     
     cursor.execute(doc_query_base, (doc_ids))
     return(dfDocsFromCursor(cursor))
+
+
+def query_backgroundWordInfo(cur, word_ids):
+    wids_format = ", ".join([str(wid) for wid in word_ids])
+    query = "SELECT * FROM words_info WHERE word_id IN ({})".format(wids_format)
+    cur.execute(query)
+    result = dfDocsFromCursor(cur)
+    return(result)
+
+
+def query_totalEntries(cur, table):
+    if table == "rssfeed_links":
+        key = "link"
+    query = "SELECT COUNT(DISTINCT {}) FROM {}".format(key, table)
+    cur.execute(query)
+    return(cur.fetchall()[0][0])
+
+
+def query_wordIDLookup(cur, words):
+    format_strings = ','.join(["%s"] * len(words))
+    query = ("SELECT id, word FROM words "
+             "WHERE word IN ({})".format(format_strings))
+    cur.execute(query, list(words))
+    cols = cur.column_names
+    word_lookup = [{cols[0] : e[0], cols[1] : e[1]} for e in cur.fetchall()]
+    word_lookup = {e['id'] : e['word'] for e in word_lookup}
+    return(word_lookup)
+
+
+def query_idWordLookup(cur, wids):
+    format_strings = ','.join(["%s"] * len(wids))
+    query = ("SELECT id, word FROM words "
+             "WHERE id IN ({})".format(format_strings))
+    cur.execute(query, list(wids))
+    cols = cur.column_names
+    word_lookup = [{cols[0] : e[0], cols[1] : e[1]} for e in cur.fetchall()]
+    word_lookup = {e['id'] : e['word'] for e in word_lookup}
+    return(word_lookup)
+
+
+def query_docsOnWords(cur, words, word_type="id", exclude_docs=set()):
+    
+    if word_type == "word":
+        word_doc_query = ("SELECT doc_bows.doc_id, doc_bows.wcount "
+                          "FROM  doc_bows LEFT JOIN words ON (doc_bows.word_id = words.id) "
+                          "WHERE words.word = '{}' ")
+    elif word_type == "id":
+        word_doc_query = ("SELECT doc_bows.doc_id, doc_bows.wcount "
+                          "FROM  doc_bows LEFT JOIN words ON (doc_bows.word_id = words.id) "
+                          "WHERE words.id = '{}' ")
+
+    doc_ids = set()
+    word_count_store = []
+    for word in words:
+        cur.execute(word_doc_query.format(word))
+        result = dfDocsFromCursor(cur)
+        result = result[[i not in exclude_docs for i in result['doc_id']]]
+        if result.shape[0] > 0:
+            doc_ids.update(set(result['doc_id']))
+            word_count_store.append({'word' : word,
+                                     'n_docs' : result.shape[0],
+                                     'n_tot' : result['wcount'].sum()})
+
+    doc_ids = [int(i) for i in doc_ids]
+    word_count_store = pandas.DataFrame(word_count_store)
+        
+    return(doc_ids, word_count_store)
+
+
+def query_wordSummaryInfo(cur, words, wtype='id', 
+                          exclude_docs=set(), include_docs=set()):
+    """
+    CREATE TABLE words_info SELECT doc_bows.word_id, 
+    COUNT(doc_bows.doc_id) as n_docs, 
+    SUM(doc_bows.wcount) as n_total  
+    FROM  doc_bows 
+    GROUP BY doc_bows.word_id;
+    
+    """
+    
+    if exclude_docs or include_docs:
+        if exclude_docs:
+            base = "AND doc_bows.doc_id NOT IN ({}) "
+            dd = exclude_docs
+        elif include_docs:
+            base = "AND doc_bows.doc_id IN ({}) "
+            dd = include_docs
+        dd = [str(int(did)) for did in dd]
+        dd = ", ".join(dd)
+        ie_where_text = base.format(dd)
+    else:
+        ie_where_text = ""
+        
+        
+    format_strings = ', '.join(['%s'] * len(words))
+    
+    if wtype=='word':
+        where_clause = "WHERE words.word IN ({}) ".format(format_strings)
+        where_clause += ie_where_text
+        query = "SELECT doc_bows.word_id, COUNT(DISTINCT doc_bows.doc_id) as n_docs, " +\
+                "SUM(doc_bows.wcount) as n_total " +\
+                "FROM  doc_bows LEFT JOIN words ON (doc_bows.word_id = words.id) " +\
+                where_clause +\
+                "GROUP BY doc_bows.word_id"
+        
+    elif wtype=='id':
+        where_clause = "WHERE doc_bows.word_id IN ({}) ".format(format_strings)
+        where_clause += ie_where_text
+        query = "SELECT doc_bows.word_id, COUNT(DISTINCT doc_bows.doc_id) as n_docs, " +\
+                "SUM(doc_bows.wcount) as n_total " +\
+                "FROM  doc_bows " +\
+                where_clause +\
+                "GROUP BY doc_bows.word_id"
+            
+    cur.execute(query, (words))
+    result = dfDocsFromCursor(cur)
+    return(result)
+
+
+def query_docBOW(cur, doc_id, word_list = []):
+    query = "SELECT word_id, wcount FROM doc_bows WHERE doc_id = {}".format(doc_id)
+    cur.execute(query)
+    cols = cur.column_names
+    bow = [{cols[0] : e[0], cols[1] : e[1]} for e in cur.fetchall()]
+    if word_list:
+        bow = [e for e in bow if e['word_id'] in word_list]
+    return(bow)
+
+
+def query_AllDocWords(cur, doc_ids):
+    doc_ids = list(doc_ids)
+    format_strings = ','.join(['%s'] * len(doc_ids))
+    query = ("SELECT DISTINCT word_id "
+             "FROM doc_bows "
+             "WHERE doc_id IN ({})".format(format_strings))
+    cur.execute(query, (doc_ids))
+    words = [e[0] for e in cur.fetchall()]
+    return(words)
         
     
 
